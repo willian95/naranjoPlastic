193b9ad4cb98423396f9404708c142a0194dc9b1
tacuato <rodriguezwillian95@gmail.com>
2019-09-30 16:18:44 -0400

    cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">Correo:</label>
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">Correo2:</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
